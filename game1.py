301425cff867770d7551cc14361c58f80035991e
alekhya555 <44549873+alekhya555@users.noreply.github.com>
2018-10-28 21:55:51 +0530

    added a comment

--- game1.py
+++ game1.py
@@ -1,4 +1,5 @@
 #get the random module which returns an unexpected value within specified range
+#this is a simple python script to a game
 import random
 print("This is a simple rock paper scissor game ")
 p1s=0
